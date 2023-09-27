348c08a0952e9eb426e2cef4821c4249f303b3c4
jlindeloef <>
2023-09-27 19:35:11 +0000

    Add def to print the board.

--- run.py
+++ run.py
@@ -4,3 +4,22 @@ from random import randint
 class Board:
     def __init__(self, board):
         self.board = board
+
+    def letters_to_numbers(self):
+        """
+        Transform letters to numbers to our boardgame column.
+        """
+        letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4}
+        return letters_to_numbers
+
+    def print_board(self):
+        """
+        Printing letters to the board.
+        Adding numbers as rows in our boardgame.
+        Creating and defying the inputs on the board.
+        """
+        print('  A B C D E')
+        row_num = 1
+        for row in self.board:
+            print("%d|%s|" % (row_num, "|".join(row)))
+            row_num += 1
