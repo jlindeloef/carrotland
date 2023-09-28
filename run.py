9133e8b46eed198801d4f7b2aed82e4019fae9b4
jlindeloef <>
2023-09-28 08:34:24 +0000

    Add code in game over def.

--- run.py
+++ run.py
@@ -64,7 +64,7 @@ class Carrots:
         """
         Tells what happens on the board when finding carrot.
         """
-         find_carrots = 0
+        find_carrots = 0
         for row in self.board:
             for column in row:
                 if column == "X":
@@ -73,6 +73,9 @@ class Carrots:
 
 
 def GameOver():
+    """
+    End game or loose
+    """
     user_input = input("Play again (Y/N)? ").upper()
     if user_input == "Y":
         Play_Game()
@@ -82,28 +85,37 @@ def GameOver():
 
 
 def Play_Game():
+    """
+    Creating board, duplicate answer,found a carrot/not found,
+    win/loose game, turns, game over.
+    """
     computer_board = Board([[" "] * 5 for i in range(5)])
     user_guess_board = Board([[" "] * 5 for i in range(5)])
     Carrots.create_carrots(computer_board)
-    # start 10 turns
     turns = 5
     while turns > 0:
         Board.print_board(user_guess_board)
-        # get user input
         user_x_row, user_y_column = Carrots.get_user_input(object)
-        # check if duplicate guess
         while user_guess_board.board[user_x_row][user_y_column] == "-"\
                 or user_guess_board.board[user_x_row][user_y_column] == "X":
             print("You searched there already")
             user_x_row, user_y_column = Carrots.get_user_input(object)
-            # check for hit or miss
-            if computer_board.board[user_x_row][user_y_column] == "X":
+        if computer_board.board[user_x_row][user_y_column] == "X":
             print("YEAH! You found a carrot!")
             user_guess_board.board[user_x_row][user_y_column] = "X"
         else:
             print("Sorry! No carrot!")
             user_guess_board.board[user_x_row][user_y_column] = "-"
-            # check for win or lose
         if Carrots.find_carrots(user_guess_board) == 5:
             print("CONGRATULATION! You found all 5 carrots! Yum! carrotcake!")
             GameOver()
+        else:
+            turns -= 1
+            print(f"You have {turns} turns remaining")
+            if turns == 0:
+                print('Game Over ')
+                GameOver()
+
+
+if __name__ == '__main__':
+    Play_Game()
