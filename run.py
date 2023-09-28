bafa8462e1338817a8633a8f332d5ef7a2d2fe99
jlindeloef <>
2023-09-28 12:49:29 +0000

    Fixed bugs in while loops in play game and get user input defs.

--- run.py
+++ run.py
@@ -40,23 +40,23 @@ class Carrots:
             while self.board[carrot_r][carrot_cl] == "X":
                 carrot_r, carrot_cl = randint(0, 4), randint(0, 4)
             self.board[carrot_r][carrot_cl] = "X"
-    
+           
     def get_user_input(self):
         """
         Get users inputs in the game and responding.
         """
         try:
             x_row = input("Look for carrot on row(1-5)...: ")
-            while x_row not in '12345':
+            if x_row not in '12345':
                 print('Not valid! Select a valid row(1-5).')
                 x_row = input("Look for carrot on row(1-5)...: ")
             y_column = input("Choose a column(A-E): ").upper()
-            while y_column not in "ABCDE":
+            if y_column not in "ABCDE":
                 print('Not valid! Select a valid column(A-E).')
                 y_column = input("Choose a column(A-E): ").upper()
             return int(x_row) - 1, \
                 Board.letters_to_numbers(self)[y_column]
-        except ValueError and KeyError:
+        except (ValueError, KeyError):
             print("Not a valid input")
             return self.get_user_input()
     
@@ -96,7 +96,7 @@ def Play_Game():
     while turns > 0:
         Board.print_board(user_guess_board)
         user_x_row, user_y_column = Carrots.get_user_input(object)
-        while user_guess_board.board[user_x_row][user_y_column] == "-"\
+        if user_guess_board.board[user_x_row][user_y_column] == "-"\
                 or user_guess_board.board[user_x_row][user_y_column] == "X":
             print("You searched there already")
             user_x_row, user_y_column = Carrots.get_user_input(object)
